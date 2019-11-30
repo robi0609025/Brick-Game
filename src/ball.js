a103ffc4440a5dbd744133634e011807501c2c88
SHEIKH AHMED SALEH (ROBI) <robi.kuet@gmail.com>
2019-12-01 03:10:36 +1100

    Ball bounce from the top of the paddle
    Ball bounce from the top of the paddle

--- src/ball.js
+++ src/ball.js
@@ -1,8 +1,8 @@
 export default class Ball {
   constructor(game) {
     this.img_ball = document.getElementById("img_ball");
-    this.gameWidth = game.gameWidth;
-    this.gameHeight = game.gameHeight;
+    this.game = game;
+    this.paddle = game.paddle;
     this.width = 15;
     this.height = 15;
     this.position = {
@@ -11,8 +11,8 @@ export default class Ball {
     };
 
     this.speed = {
-      x: 2,
-      y: 2
+      x: 3,
+      y: 3
     };
   }
 
@@ -29,16 +29,26 @@ export default class Ball {
   update(deltaTime) {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
-
-    if (this.position.x + this.width > this.gameWidth || this.position.x < 0) {
+    //collision with the side wall
+    if (
+      this.position.x + this.width > this.game.gameWidth ||
+      this.position.x < 0
+    ) {
       this.speed.x = -this.speed.x;
     }
-
+    //collision with bottom or top of the wall
     if (
-      this.position.y + this.height > this.gameHeight ||
+      this.position.y + this.height > this.game.gameHeight ||
       this.position.y < 0
     ) {
       this.speed.y = -this.speed.y;
+    }
+    //collision with the paddle
+    let bottomOfBall = this.position.y + this.height;
+    let topOfPaddle = this.game.paddle.position.y;
+    if (bottomOfBall >= topOfPaddle) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.height;
     }
   }
 }
